1db2833a5e144353ba69ce1a329c02ab5f9c0e78
[CarlosFdezJim] <57524609+CarlosFdezJim@users.noreply.github.com>
2020-04-01 21:05:16 +0200

    DIA 1

--- cristobook.cpp
+++ cristobook.cpp
@@ -1,10 +1,8 @@
-// 
-//	Practica2T.cpp
+//
 //
 // Created by Carlos Fdez
 //
 //
-
 
 //**********COLOR**********//
 
@@ -24,8 +22,85 @@ using namespace std;
 
 //-------------------------------------------------------------//
 
-void Menu(){
+
+
+
+
+
+/**
+ * @brief Este módulo sólo muestra por pantalla el menú de opciones que tenemos disponibles.
+ * @post El usuario seleccionará la opción que el desee.
+ * @version 1.0
+ * @author Carlos Fdez.
+ */
+void MostrarMenu(){
+
+	cout << BLUE <<"/***************************************"<< endl;
+	cout << "****	BIENVENIDOS A CRISTOBOOK   *****" << endl;
+	cout << "****************************************/ ";
+	cout << "\nPulse la tecla que desee para realizar una de las siguientes acciones: ";
+	cout << "\n[1]  Crear tabla Usuarios. ";
+	cout << "\n[2]  Eliminar tabla Usuarios. ";
+	cout << "\n[3]  Imprimir tabla Usuarios. ";
+	cout << "\n[4]  Insertar Usuario en tabla Usuarios. ";
+	cout << "\n[5]  Eliminar Usuario en tabla Usuarios. ";
+	cout << "\n[6]  Buscar Usuario por Atributo Login en tabla Usuarios. "; //(utilizar la búsqueda secuencial)
+	cout << "\n[7]  Ordenar tabla Usuarios por Atributo totalFotosUsuario y por atributo login. ";
+	cout << "\n[8]  Añadir Fotografía a Usuario. ";
+	cout << "\n[9]  Eliminar Fotografía de un Usuario. ";
+	cout << "\n[10] Imprimir las Fotografías de un Usuario. ";
+	cout << "\n[11] Salir. " << DEFAULT << endl;
 	
+}
+/**
+ * @brief Este módulo sirve para acceder a la opción del menu mostrado previamente.
+ * @param 
+ * @param 
+ * @param 
+ * @pre El menú deberá de estar hecho inicialmente.
+ * @post Mostramos el menú y vamos a la opción seleccionada por el usuario.
+ * @version 2.5
+ * @author Carlos Fdez.
+ */
+void MenuInicio(){
+
+	//Declaración de variables.
+	int opcion=0;
+	
+	while(opcion!=11){
+		MostrarMenu();
+		cout << "\nEliga usted el programa que desea ejecutar: " << endl;		
+		cin >> opcion;
+	
+			switch (opcion){
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:		
+					break;
+				case 5:		
+					break;
+				case 6:		
+					break;
+				case 7:		
+					break;
+				case 8:		
+					break;
+				case 9:		
+					break;
+				case 10:		
+					break;
+				case 11:
+					cout << BLUE << "----------SALIENDO----------\n" << DEFAULT << endl;
+					break;
+				default:
+					cout << YELLOW << "\nLa opción seleccionada no es correcta. " << DEFAULT << endl;
+			}
+		
+	}
 }
 
 int main(){
@@ -36,7 +111,7 @@ int main(){
  
  
  
- 
+	MenuInicio();
  
  
  
