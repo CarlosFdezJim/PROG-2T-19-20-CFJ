250b2bb32da1c5e4ead626b3984dd227c8e46e04
[CarlosFdezJim] <57524609+CarlosFdezJim@users.noreply.github.com>
2020-04-01 14:37:24 +0200

    Prueba

---
+++ Practica2T.cpp
@@ -0,0 +1,39 @@
+//
+//
+// Created by Carlos Fdez
+//
+//
+
+//**********COLOR**********//
+
+#define DEFAULT "\033[1;0m"
+#define GREY "\033[1;30m"
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define PURPLE "\033[1;35m"
+#define CYAN "\033[1;36m"
+#define ERROR "\033[1;41m"
+//************************//
+
+#include <iostream>
+using namespace std;
+
+//-------------------------------------------------------------//
+
+
+int main(){
+ 
+	//Declaración de variables.
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
